2a79a8801c0a73e0a9ee18e4c1f6064bf76a8e64
Rajesh Gandhudi <Rajesh.Gandhudi@uk.ey.com>
2021-08-21 00:10:53 +0100

    final terraform

---
+++ providers.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  region     = "eu-west-2"
+  access_key = "AKIAUCASE73WTKZGBIH2"
+  secret_key = "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"
+}
---
+++ resorces.tf
@@ -0,0 +1,4 @@
+resource "aws_instance" "ec2"{
+  ami           = "ami-0d26eb3972b7f8c96"
+  instance_type = var.instancetype
+}
